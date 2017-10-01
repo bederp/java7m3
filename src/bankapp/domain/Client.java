7355cc9bed3b21b431aa2dedfe1f4889acd47b59
Paweł Beder <pawel.beder@gmail.com>
2017-10-01 18:37:16 +0200

    Added word document and bank app

---
+++ src/bankapp/domain/Client.java
@@ -0,0 +1,48 @@
+package bankapp.domain;
+
+import java.util.Arrays;
+
+public class Client {
+    private String name;
+    private Gender gender;
+
+    private Account[] accounts;
+    private int countOfAccounts;
+
+    public Client(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+        this.accounts = new Account[100];
+        countOfAccounts = 0;
+    }
+
+    public void addAccount(final Account account) {
+        accounts[countOfAccounts++] = account;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Account[] getAccounts() {
+        return Arrays.copyOf(accounts, accounts.length);
+    }
+
+    private String getClientGreeting() {
+        if (gender != null) {
+            return gender.getGreeting() + " " + name;
+        } else {
+            return name;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getClientGreeting();
+    }
+
+}
---
+++ src/bankapp/domain/SavingAccount.java
@@ -0,0 +1,13 @@
+package bankapp.domain;
+
+
+public class SavingAccount extends AbstractAccount {
+    public SavingAccount(int id, double balance) {
+        super(id, balance);
+    }
+
+    public double maximumAmountToWithdraw() {
+        return getBalance();
+    }
+
+}
